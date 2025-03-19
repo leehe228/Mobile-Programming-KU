8e37d717730e13e20f1eee05c7b7cda1e6c77aeb
Hoeun Lee <hoeunlee228@gmail.com>
2025-03-19 13:17:09 +0900

    Update ToDoMain.kt

--- MobileProgrammingPractice/eweek03a/src/main/java/com/leehe228/eweek03a/ToDoMain.kt
+++ MobileProgrammingPractice/eweek03a/src/main/java/com/leehe228/eweek03a/ToDoMain.kt
@@ -1,12 +1,35 @@
 package com.leehe228.eweek03a
 
 import com.leehe228.eweek03a.model.Item
+import com.leehe228.eweek03a.model.ToDoListFactory
 import com.leehe228.eweek03a.model.ToDoStatus
 
 fun main() {
-    val item1 = Item("모프 공부하기1", "03-19 12:50")
-    val item2 = Item("모프 공부하기2", "03-19 12:50", ToDoStatus.COMPLETED)
+    // Practice 01
+//    val item1 = Item("모프 공부하기1", "03-19 12:50")
+//    val item2 = Item("모프 공부하기2", "03-19 12:50", ToDoStatus.COMPLETED)
+//
+//    println(item1)
+//    println(item2)
 
-    println(item1)
-    println(item2)
+    // Practice 02
+//    val todoList = ToDoListFactory.makeToDoList()
+//
+//    if (todoList.isEmpty()) {
+//        println("등록된 일정이 없습니다.")
+//    } else {
+//        todoList.forEachIndexed { index, item ->
+//            println("$index : $item")
+//        }
+//    }
+
+    // Practice 03
+    // val todoService = ToDoService(mutableListOf<Item>())
+    val todoService = ToDoService(ToDoListFactory.makeToDoList())
+
+    // e.g.
+    todoService.addContent("모프 공부하기7")
+
+    // print all
+    todoService.listAllToDo()
 }
