3176b21a3fce2a37f338a483c0279cd5614dd364
Hoeun Lee <hoeunlee228@gmail.com>
2025-03-24 12:54:15 +0900

    Add Practice Week4 Project

---
+++ PracticeWeek04E/app/src/main/java/com/leehe228/practiceweek04e/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.leehe228.practiceweek04e
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.leehe228.practiceweek04e.ui.theme.PracticeWeek04ETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeWeek04ETheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticeWeek04ETheme {
+        Greeting("Android")
+    }
+}
