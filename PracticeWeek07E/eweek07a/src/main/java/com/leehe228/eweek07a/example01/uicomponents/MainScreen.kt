a9218594ef25647ee137e3ada78203296a6ddf76
Hoeun Lee <hoeunlee228@gmail.com>
2025-04-16 13:15:50 +0900

    Add Backstack Log

--- PracticeWeek07E/eweek07a/src/main/java/com/leehe228/eweek07a/example01/uicomponents/MainScreen.kt
+++ PracticeWeek07E/eweek07a/src/main/java/com/leehe228/eweek07a/example01/uicomponents/MainScreen.kt
@@ -1,12 +1,20 @@
 package com.leehe228.eweek07a.example01.uicomponents
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.leehe228.eweek07a.example01.navGraph.NavGraph
 
+@SuppressLint("RestrictedApi")
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
+    navController.addOnDestinationChangedListener { _, _, _ ->
+        navController.currentBackStack.value.forEachIndexed { index, entry ->
+            Log.d("backstack", "$index: ${entry.destination.route}")
+        }
+    }
     NavGraph(navController = navController)
 }
