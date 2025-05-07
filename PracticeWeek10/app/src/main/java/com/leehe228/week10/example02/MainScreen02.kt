dd41dd72ac5627706c4204c47b24d68711c4d8aa
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-07 16:03:52 +0900

    Add Permission Grant Dialog

--- PracticeWeek10/app/src/main/java/com/leehe228/week10/example02/MainScreen02.kt
+++ PracticeWeek10/app/src/main/java/com/leehe228/week10/example02/MainScreen02.kt
@@ -1,7 +1,10 @@
 package com.leehe228.week10.example02
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
+import android.provider.Settings
+import android.util.Log.v
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,16 +12,80 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import com.leehe228.week10.functions.makeCall
+import com.leehe228.week10.uicomponents.RationaleDialog
+import com.leehe228.week10.uicomponents.SettingsDialog
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MainScreen02(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    val callPermissionState = rememberPermissionState(permission = Manifest.permission.CALL_PHONE)
+    var showCallDialog by remember { mutableStateOf(false) }
+    var showSettingDialog by remember { mutableStateOf(false) }
+    var permissionConfirm by remember { mutableStateOf(false) } // 권한 승인을 한 번이라도 받았는지
+
+    fun requestCallPermission() {
+        when {
+            callPermissionState.status.isGranted -> {
+                // 권한 승인 받았으면 전화 걸기
+                makeCall(context)
+            }
+
+            callPermissionState.status.shouldShowRationale -> {
+                // 설명 보여주기
+                showCallDialog = true
+            }
+
+            else -> {
+                if (permissionConfirm) {
+                    showSettingDialog = true
+                } else {
+                    // 권한 요청이 처음인 경우
+                    permissionConfirm = true
+                    callPermissionState.launchPermissionRequest() // 요청
+                }
+            }
+        }
+    }
+
+    if (showCallDialog) {
+        RationaleDialog(
+            onDismiss = { showCallDialog = false },
+            onConfirm = {
+                showCallDialog = false
+                callPermissionState.launchPermissionRequest()
+            }
+        )
+    }
+    
+    if (showSettingDialog) {
+        SettingsDialog(
+            onDismiss = { showSettingDialog = false },
+            onGoToSettings = {
+                showSettingDialog = false
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = "package:${context.packageName}".toUri()
+                }
+                context.startActivity(intent)
+            }
+        )
+    }
+
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -55,20 +122,13 @@ fun MainScreen02(modifier: Modifier = Modifier) {
         }
 
         Button(onClick = {
-            val number = "tel:010-1234-1234".toUri()
-            val callIntent = Intent(Intent.ACTION_DIAL, number)
-            context.startActivity(callIntent)
+            requestCallPermission()
+//            val number = "tel:010-1234-1234".toUri()
+//            val callIntent = Intent(Intent.ACTION_DIAL, number)
+//            context.startActivity(callIntent)
         }, modifier = Modifier.width(200.dp)) {
             Text("전화 걸기")
         }
-
-        /* Button(onClick = {
-            val number = Uri.parse("tel:010-1234-1234")
-            val callIntent = Intent(Intent.ACTION_CALL, number) // 권한 필요
-            context.startActivity(callIntent)
-        }, modifier = Modifier.width(200.dp)) {
-            Text("전화 걸기")
-        } */
     }
 }
 
