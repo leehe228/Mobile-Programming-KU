3156b72da99d97d58836a4422ab271aec0f93ae0
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-07 15:13:58 +0900

    Practice - Add Personal Image in Drawer

--- PracticeWeek09/app/src/main/java/com/leehe228/week09/example02/uicomponents/DrawerContent.kt
+++ PracticeWeek09/app/src/main/java/com/leehe228/week09/example02/uicomponents/DrawerContent.kt
@@ -1,6 +1,9 @@
 package com.leehe228.week09.example02.uicomponents
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,9 +14,12 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.leehe228.week09.R
 
 @Composable
 fun ColumnScope.DrawerContent(modifier: Modifier = Modifier) {
@@ -22,6 +28,18 @@ fun ColumnScope.DrawerContent(modifier: Modifier = Modifier) {
         modifier = Modifier.padding(16.dp),
         fontSize = 20.sp
     )
+
+    // 실습 - 본인 사진 추가
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.image),
+            contentDescription = ""
+        )
+    }
 
     Text(
         text = "leehe228@konkuk.ac.kr",
