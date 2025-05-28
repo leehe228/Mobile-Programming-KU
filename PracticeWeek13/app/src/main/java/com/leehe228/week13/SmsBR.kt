cdb3743a28f894db60679333a8e729e916285b8a
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-28 13:15:29 +0900

    Week13 SMS Receiver

---
+++ PracticeWeek13/app/src/main/java/com/leehe228/week13/SmsBR.kt
@@ -0,0 +1,11 @@
+package com.leehe228.week13
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsBR : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
